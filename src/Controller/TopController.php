ce0c84bab3161aab3ec06d1c94de04154769d923
yonekatsuya <seiryo.soccer.ky@i.softbank.jp>
2020-07-24 03:24:48 +0900

    トップページ表示用コントローラ・ビューテンプレートの作成

---
+++ src/Controller/TopController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+class TopController extends AppController {
+  public function initialize() {
+    $this->name = 'Top';
+    $this->viewBuilder()->layout('Main');
+  }
+
+  public function index() {
+
+  }
+}
---
+++ src/Template/Layout/main.ctp
@@ -0,0 +1,1 @@
+fjliaji;fac
