ea41ea01fd441a702fb7c05ae4e3b4e897a499eb
yonekatsuya <seiryo.soccer.ky@i.softbank.jp>
2020-07-26 01:56:03 +0900

    config.ymlの内容を、ブログ記事に沿って大幅修正。CircleCIの挙動確認

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -76,6 +76,11 @@ try {
     exit($e->getMessage() . "\n");
 }
 
+if (file_exists(__DIR__ . DS . '.env')) {
+    $dotEnv = new Dotenv\Dotenv(__DIR__);
+    $dotEnv->load();
+}
+
 /*
  * Load an environment local configuration file.
  * You can use a file like app_local.php to provide local overrides to your
